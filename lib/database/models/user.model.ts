68f40dd96e2ac636bc71bc705a5b648c5a936b41
Kujtim Prenku <prenkukujtim.kp@gmail.com>
2024-03-03 10:04:21 +0100

    Database + Models Setup.

---
+++ lib/database/models/user.model.ts
@@ -0,0 +1,29 @@
+import mongoose, { model, models, Schema } from "mongoose";
+
+export interface IUser extends Document {
+  clercId: string,
+  email: string,
+  username: string,
+  photo: string,
+  firstName: string,
+  lastName: string,
+  planId: number,
+  creditBalance: number,
+
+}
+
+const UserSchema = new Schema({
+  clerkId: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true },
+  photo: { type: String, required: true },
+  firstName: { type: String },
+  lastName: { type: String },
+  plainId: { type: Number, default: 1 },
+  creditBalance: { type: Number, default: 10 },
+
+})
+
+const User = models?.User || model("User", UserSchema);
+
+export default User;
