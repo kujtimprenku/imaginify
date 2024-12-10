f7da1ef083c5568ec0d9bf81afddd6d754d48a4f
Kujtim Prenku <prenkukujtim.kp@gmail.com>
2024-12-10 13:26:50 +0100

    Log the result.

--- components/test.tsx
+++ components/test.tsx
@@ -1,18 +1,22 @@
 "use client"
 
 export const Test = () => {
+
+    const handleShare = async () => {
+        if (navigator?.share) {
+            navigator.share({
+                title: "MDN",
+                text: "Learn web development on MDN!",
+                url: "https://developer.mozilla.org",
+            }).then((result) => {
+                console.log({result})})
+        } else  {
+            alert('wrong')
+        }
+    }
     return (
         <button onClick={() => {
 
-            if (navigator?.share) {
-                navigator.share({
-                    title: "MDN",
-                    text: "Learn web development on MDN!",
-                    url: "https://developer.mozilla.org",
-                })
-            } else  {
-                alert('wrong')
-            }
 
         }}>Share</button>
     )
